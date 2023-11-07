b0d638cba811071a227acfe23090c9ce458829f6
pjh0904 <pjh0904@jainwon.com>
2023-11-07 09:51:47 +0900

    git init

---
+++ src/main/java/com/example/dbstudy/DbStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbstudy/DbStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
